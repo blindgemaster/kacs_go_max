f84af16233b91a73363f278d447335962c4b829c
khizarasim <54252989+khizarasim@users.noreply.github.com>
2019-09-03 20:25:45 +0500

    New file created

--- Problem_C_Moist.py
+++ Problem_C_Moist.py
@@ -1,7 +1,8 @@
 dictionary = {'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4, 'g': 7, 'f': 6, 'i': 9, 'h': 8, 'k': 11, 'j': 10, 'm': 13, 'l': 12, 'o': 15, 'n': 14, 'q': 17, 'p': 16, 's': 19, 'r': 18, 'u': 21, 't': 20, 'w': 23, 'v': 22, 'y': 25, 'x': 24, 'z': 26}
 counter = 1
-with open('D:/Downloads/C-small-practice-2.out', 'w') as file2:
-    with open('D:/Downloads/C-small-practice-2.in') as file:
+address = input('Please enter the address without filetype:  ')
+with open(f'{address}.out', 'w') as file2:
+    with open(f'{address}.in') as file:
         test_counter = int(file.readline())
         while not counter == test_counter+1:
             list1 = []
